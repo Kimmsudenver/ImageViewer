e30c857a888192e052515e58841a2cb78e27af33
Guy <guy.grigsby@gmail.com>
2014-09-29 22:25:15 -0600

    merged save text with save image commits

--- src/controller/ButtonController.java
+++ src/controller/ButtonController.java
@@ -1,9 +1,11 @@
 package controller;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
